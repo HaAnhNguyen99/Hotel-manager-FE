6210de720996a6e0fd8bef1106d8e7da1eed23dc
Ha anh <haanhnguyen1221@gmail.com>
2025-03-31 02:01:29 +0700

    Add: logo to dashboard header

--- src/components/Dashboard/HotelInfo/HotelInfo.tsx
+++ src/components/Dashboard/HotelInfo/HotelInfo.tsx
@@ -1,6 +1,8 @@
+import logo from "@/assets/images/logo v2 dark.jpeg";
+
 const HotelInfo = () => {
   return (
-    <div className="flex justify-between items-center font-bigShoulders text-right">
+    <div className="flex gap-3 justify-between items-center font-bigShoulders text-right">
       <div>
         <h1 className="text-2xl font-extrabold font-bigShoulders italic">
           Khách sạn Phương Trang
@@ -8,6 +10,9 @@ const HotelInfo = () => {
         <p className="italic font-thin mt-2">
           53 Lê Vĩnh Hoà, Phường Phú Thọ Hoà, Quận Tân Phú, TP HCM
         </p>
+      </div>
+      <div className="aspect-square w-14 rounded-2xl overflow-hidden">
+        <img src={logo} alt="hotel Phuong trang loo" className="object-cover" />
       </div>
     </div>
   );
