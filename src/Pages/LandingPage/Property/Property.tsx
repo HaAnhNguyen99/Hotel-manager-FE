303597d0a397f0f34f38f01f9f998b52549c2249
NguyenHaAnh <haanhnguyen1221@gmail.com>
2025-05-27 22:04:44 +0700

    feat: implement GSAP animations for Property component with scroll-triggered effects

--- src/Pages/LandingPage/Property/Property.tsx
+++ src/Pages/LandingPage/Property/Property.tsx
@@ -1,4 +1,8 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { Bath, BedSingle, Car, PawPrint } from "lucide-react";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/all";
 
 const Property = () => {
   const subtitle = "Về chúng tôi";
@@ -27,8 +31,46 @@ const Property = () => {
       des: "Nước nóng, nước lạnh, máy lạnh, máy sấy tóc",
     },
   ];
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const propertyRef = useRef<HTMLDivElement>(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(
+    () => {
+      if (!propertyRef.current) return;
+
+      const items = gsap.utils.toArray(".property-item", propertyRef.current);
+
+      gsap.fromTo(
+        items,
+        {
+          y: 50,
+          autoAlpha: 0,
+        },
+        {
+          y: 0,
+          autoAlpha: 1,
+          duration: 0.6,
+          ease: "power2.out",
+          stagger: 0.2,
+          scrollTrigger: {
+            trigger: propertyRef.current,
+            start: "top 80%",
+            end: "bottom 20%",
+            toggleActions: "play none none reverse",
+          },
+        }
+      );
+    },
+    { scope: propertyRef }
+  );
+
   return (
-    <section id="property" className="px-4 py-4 mt-14 sm:mt-0">
+    <section
+      id="property"
+      className="px-4 py-4 mt-14 sm:mt-0"
+      ref={containerRef}>
       <div className="p-5 text-center">
         <h3 className="font-bold text-lg mb-4 font-sans uppercase tracking-wider text-landing">
           {subtitle}
@@ -38,11 +80,13 @@ const Property = () => {
           <p className="text-grey-secondaryLight">{des}</p>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-screen-2xl mx-auto items-center gap-8 text-sm md:w-full ">
+      <div
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-screen-2xl mx-auto items-center gap-8 text-sm md:w-full"
+        ref={propertyRef}>
         {data.map((item, index) => (
           <div
             key={index}
-            className=" flex border border-red-100 gap-2 h-full w-full rounded-lg text-center py-6 p-2 bg-[#f9f7f4] flex-col">
+            className="property-item flex border border-red-100 gap-2 h-full w-full rounded-lg text-center py-6 p-2 bg-[#f9f7f4] flex-col">
             <div className="self-center border border-landing-bgBlack p-1 w-fit h-fit rounded-full mb-5">
               <div className="flex justify-center items-center bg-landing-bgBlack p-3 rounded-full">
                 <item.icon className="w-12 h-12 text-landing-primaryLight" />
